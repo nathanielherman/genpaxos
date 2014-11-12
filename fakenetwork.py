d4dd65dae4978e8d455405069ad1f759c8bf2f44
Nathaniel Herman <nherman@college.harvard.edu>
2014-11-12 13:58:10 -0500

    fakenetwork changes

--- fakenetwork.py
+++ fakenetwork.py
@@ -1,14 +1,25 @@
 
 class FakeNetwork(object):
-    # config should be an array of EventHandler's indexed by their certifier id
-    def __init__(self, config):
+    # config should be an array of Network's indexed by their certifier id
+    def __init__(self, config, me):
         self.config = config
+        # our eventhandler
+        self.me = me
 
     def sendAll(self, msg, item):
-        for n in self.config:
-            self.send(n, msg, item)
+        resps = []
+        for node in self.config:
+            resp = self.send(node, msg, item)
+            resps.append(resp)
+        return resps
+
+    def receive(self, msg, item):
+        resp = self.me.request(msg, item)
+        return resp
 
     # send does an actual network "send"
     def send(self, node, msg, item):
-        # our fake network calls request directly
-        self.config[node].request(msg, item)
+        # our fake network calls a node's network.receive() directly
+        resp = self.config[node].receive(msg, item)
+        # retry if returns None?
+        return resp
